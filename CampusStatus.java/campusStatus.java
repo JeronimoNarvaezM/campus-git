e153a2def5f31cbd3ce63802e1ee028f446c1f58
Jeronimo Narvaez <jeronimo322607220@gmail.com>
2026-09-07 20:24:29 -0500

    Se agrego la logica de la clase campusStatus

---
+++ CampusStatus.java/campusStatus.java
@@ -0,0 +1,25 @@
+public class CampusStatus {
+    private String status;
+    private String message;
+
+    public CampusStatus(String status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
